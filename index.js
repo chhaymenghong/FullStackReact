fe0bc6a000859bee6462e393d643d77bf356d6ca
chhaymenghong <chhaymenghong@yahoo.com>
2017-09-05 13:30:21 -0700

    Add express server

---
+++ index.js
@@ -0,0 +1,13 @@
+// require belongs to commonjs spec that Node uses to import modules
+// node hasn't implemented the "import" feature belong to es6 yet
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send({hi: 'dfsdfd'});
+   console.dir(this);
+});
+
+// since we are using Heroku to host this server, we need to accept the port number given by Heroku
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
