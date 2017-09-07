578f95d1b999caeb51709dbc4f319a26c754f5d1
chhaymenghong <chhaymenghong@yahoo.com>
2017-09-06 21:22:40 -0700

    Add mongoose to work with MongoDB on mLab

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 // Require belongs to commonjs spec that Node uses to import modules
 // node hasn't implemented the "import" feature belong to es6 yet
 const express = require('express');
+const configKey = require('./config/keys');
+// Add DB connection
+const mongoose = require('mongoose');
+mongoose.connect(configKey.mongooseUri);
 
 // Run the passport configuration
 require('./services/passport');
