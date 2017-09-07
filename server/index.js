0db7073714571e36f4ba4d50178e2741e27b6cc5
chhaymenghong <chhaymenghong@yahoo.com>
2017-09-06 22:00:58 -0700

    Add user creation to OAuth process

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
-// Require belongs to commonjs spec that Node uses to import modules
-// node hasn't implemented the "import" feature belong to es6 yet
-const express = require('express');
+// Get Config Key
 const configKey = require('./config/keys');
-// Add DB connection
-const mongoose = require('mongoose');
-mongoose.connect(configKey.mongooseUri);
+
+// Load User collection into mongoose
+require('./models/User');
 
 // Run the passport configuration
 require('./services/passport');
 
+// Add DB connection
+const mongoose = require('mongoose');
+mongoose.connect(configKey.mongooseUri);
+
+// Bootstrap app
+// Require belongs to commonjs spec that Node uses to import modules
+// node hasn't implemented the "import" feature belong to es6 yet
+const express = require('express');
 const app = express();
 require('./routes/authRoutes')(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('users'); // retrieve this collection
 
 // Tell passport to use google oauth for authentication
 passport.use(
@@ -14,9 +16,17 @@ passport.use(
         // Get executed when a user is actually executed. we can store this on the database
         // to link this token to a user for later use.
         (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(refreshToken);
-            console.log(profile);
-            console.log('hi');
+            User.findOne({googleId: profile.id})
+                .then( existingUser => {
+                    if ( existingUser ) {
+                        // first arg: no error, second arg: tell passport that this is the user that we just created
+                        done(null, existingUser);
+                    } else {
+
+                        new User({ googleId: profile.id }) // create a user model instance
+                            .save() //save it to db
+                            .then(user => done(null, user)); // finish saving, so we need to call done to finish the OAuth process
+                    }
+                });
         } )
 );
